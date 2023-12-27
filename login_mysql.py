137d14ba3db2b5ba5e4bec56410636685cfdfbaf
Dmeows <habaokhanh239@gmail.com>
2023-12-27 16:37:31 +0700

    mysql login

---
+++ login_mysql.py
@@ -0,0 +1,11 @@
+# URL = "jdbc:mysql://localhost:3307/data_engineering" 
+# DRIVER = "com.mysql.cj.jdbc.Driver"
+# USER = "root"
+# PASSWORD = "1"
+USER = 'root'
+PASSWORD = '1'
+HOST = 'localhost'
+PORT = '3307'
+DB_NAME = 'data_engineering'
+URL = 'jdbc:mysql://' + HOST + ':' + PORT + '/' + DB_NAME
+DRIVER = "com.mysql.cj.jdbc.Driver"
